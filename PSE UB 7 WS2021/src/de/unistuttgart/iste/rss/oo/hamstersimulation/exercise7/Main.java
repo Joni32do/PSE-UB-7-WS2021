a43e321c4963f8e68f9a8427986f2365ef0de25b
joni32_do <59749440+Joni32do@users.noreply.github.com>
2020-10-30 21:43:59 +0100

    Added some commentary

--- PSE UB 7 WS2021/src/de/unistuttgart/iste/rss/oo/hamstersimulation/exercise7/Main.java
+++ PSE UB 7 WS2021/src/de/unistuttgart/iste/rss/oo/hamstersimulation/exercise7/Main.java
@@ -16,24 +16,30 @@ import de.unistuttgart.iste.rss.oo.hamstersimulator.ui.javafx.JavaFXUI;
 
 /**
  * Class demonstrating the intended use of the hamster simulator API.
+ * 
  * @author Steffen Becker
  *
  */
 public final class Main {
-	
-	
-	
-	public final static Location targetLocation = new Location(1,1);
+
+	public final static Location targetLocation = new Location(1, 1);
+
 	public final static int targetAmountGrain = 5;
+
+	public final static Direction targetDirection = Direction.EAST;
+
+	public final static String targetString = "Ja";
 	
 	
-	
-	
-	
-	
+
+	/**
+	 * Method in which the students should find a way to move Paule around the Territory
+	 * and gather five grains to finally return back in to his cave and fullfil all his requirements
+	 * 
+	 * @param paule
+	 */
 	public static void studentTask(Hamster paule) {
-		
-		
+
 		moveTillWall(paule);
 		turnRight(paule);
 		moveTillWall(paule);
@@ -53,11 +59,9 @@ public final class Main {
 		moveTillWall(paule);
 		pickAll(paule);
 		turnAround(paule);
-		
-		
-		//interestingly you could do this now backwards by doing everything in reverse query
-		
-		
+
+		// interestingly you could do this backwards by doing everything in reverse
+
 		moveTillWall(paule);
 		turnRight(paule);
 		moveTillWall(paule);
@@ -76,239 +80,182 @@ public final class Main {
 		paule.turnLeft();
 		moveTillWall(paule);
 		turnAround(paule);
-		
-		
-		
-		
-		
-		
-		
-		
-		//checks if the objective is achieved
+
+		// checks if the objective is achieved
 		isFinished(paule);
 	}
-	
-	
-	
-	
-	
 
-
-
-	//possible helpful commands to add
+	// possible helpful commands to add
 	
+	
+	
+	/**
+	 * Turns paule around
+	 * @ensures \old paule.getDirection() == Direction.NORTH -> \neww paule.getDirection() == Direction.SOUTH
+	 * @param paule a Hamster object
+	 */
 	private static void turnAround(Hamster paule) {
 		paule.turnLeft();
 		paule.turnLeft();
 	}
 
-
-
+	/**
+	 * Paule moves to the next Wall or the end of the territory according to the Direction
+	 * he looks at
+	 * @param paule
+	 */
 	public static void moveTillWall(Hamster paule) {
-		while(paule.frontIsClear()) {
+		/**
+		 * @variant decreasing tiles till next wall
+		 * @invariant paule.frontIsClear() -> paule moved one tile
+		 */
+		while (paule.frontIsClear()) {
 			paule.move();
 		}
 	}
-	
+
+	/**
+	 * Turns paule right
+	 * @param paule
+	 */
 	public static void turnRight(Hamster paule) {
 		paule.turnLeft();
 		paule.turnLeft();
 		paule.turnLeft();
 	}
-	
-
-	private static void pickAll(Hamster paule) {
-		while(paule.grainAvailable()) {
-			paule.pickGrain();
-		}
-		
-	}
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-    private static void isFinished(Hamster paule) {
-
-    	if(paule.readString("Sind sie wirklich fertig?").equals("Ja")){
-    		if(paule.getLocation().equals(targetLocation)) {
-    			if(paule.getDirection().equals(Direction.EAST)) {
-    			for(int i = 0; i < targetAmountGrain; i++) {
-    				if(!paule.mouthEmpty()) {
-    					paule.putGrain();
-    				} else {
-    					paule.write("Oh nein ich habe zu wenig Körner, jetzt muss ich Seppuku betreiben, "
-    							+ "um den Namen meiner Familie wieder rein zu waschen!");
-    					break;
-    				}
-    			}
-    			} else {
-    				paule.write("Falsche Richtung!");
-    			}
-    		} else {
-    			paule.write("Falsches Feld!");
-    		}
-    	} else {
-    		paule.write("Dann nerv nicht!");
-    	}
-		
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 	/**
-     * A constant containing the location of an example grain on the territory.
-     */
-    private static final Location GRAIN_LOCATION = Location.from(1, 3);
+	 * Paule picks up all grains on a tile
+	 * @param paule
+	 */
+	private static void pickAll(Hamster paule) {
+		while (paule.grainAvailable()) {
+			paule.pickGrain();
+		}
 
-    /**
-     * Row count of the territory.
-     */
-    private static final int MAX_ROW_COUNT = 8;
+	}
+	
+	
+	
+	
 
-    /**
-     * Column count of the territory.
-     */
-    private static final int MAX_COLUMN_COUNT = 8;
+	/**
+	 * Checks if all requirements are achieved
+	 * @param paule
+	 */
+	private static void isFinished(Hamster paule) {
 
-    /**
-     * Size of the example's territory.
-     */
-    private static final Size EXAMPLE_TERRITORY_SIZE = new Size(MAX_ROW_COUNT, MAX_COLUMN_COUNT);
+		checkMessage(paule);
+		checkLocation(paule);
+		checkDirection(paule);
+		checkGrains(paule);
 
-    /**
-     * Main routine of the example class.
-     * @param args Command line arguments, not used here.
-     */
-    public static void main(final String...args) {
-        final HamsterGame hamsterGame = new HamsterGame();
+	}
+
+	private static void checkMessage(Hamster paule) {
+		if (paule.readString("Sind sie wirklich fertig?").equals(targetString)) {
+		} else {
+			paule.write("Dann nerv nicht!");
+		}
+	}
+
+	private static void checkLocation(Hamster paule) {
+		if (paule.getLocation().equals(targetLocation)) {
+			
+		} else {
+			paule.write("Falsches Feld!");
+		}
+	}
+
+	private static void checkDirection(Hamster paule) {
+		if (paule.getDirection().equals(targetDirection)) {
+			
+		} else {
+			paule.write("Falsche Richtung!");
+		}
+	}
+
+	private static void checkGrains(Hamster paule) {
+		for (int i = 0; i < targetAmountGrain; i++) {
+			if (!paule.mouthEmpty()) {
+				paule.putGrain();
+			} else {
+				paule.write("Oh nein ich habe zu wenig Körner");
+				break;
+			}
+		}
+	}
+	
+	
+	
+
+	/**
+	 * Main routine of the example class.
+	 * 
+	 * @param args Command line arguments, not used here.
+	 */
+	public static void main(final String... args) {
+		final HamsterGame hamsterGame = new HamsterGame();
 
 //        hamsterGame.initialize(createTerritory(hamsterGame));
-        
-        File terFile = new File ("territories/territory_cave.ter");
-        try {
-        	InputStream targetStream = new FileInputStream(terFile);
-            hamsterGame.initialize(targetStream);
-        } catch (IOException ex){
-            throw new RuntimeException(ex);
-        }
-        initUI(hamsterGame);
-        runGameCycle(hamsterGame);
-    }
 
-    /**
-     * Starts, runs and finally stops the hamster game.
-     * @param hamsterGame The hamster game to run, must not be null.
-     */
-    private static void runGameCycle(final HamsterGame hamsterGame) {
-        assert hamsterGame != null;
+		File terFile = new File("territories/territory_cave.ter");
+		try {
+			InputStream targetStream = new FileInputStream(terFile);
+			hamsterGame.initialize(targetStream);
+		} catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
+		initUI(hamsterGame);
+		runGameCycle(hamsterGame);
+	}
 
-        hamsterGame.startGame(false);
-        hamsterGame.runGame(Main::runHamster);
-        hamsterGame.stopGame();
-    }
+	/**
+	 * Starts, runs and finally stops the hamster game.
+	 * 
+	 * @param hamsterGame The hamster game to run, must not be null.
+	 */
+	private static void runGameCycle(final HamsterGame hamsterGame) {
+		assert hamsterGame != null;
 
-    /** Setup routine which configures the UI appropriately.
-     * @param hamsterGame The hamster game to run, must not be null.
-     */
-    private static void initUI(final HamsterGame hamsterGame) {
-        assert hamsterGame != null;
+		hamsterGame.startGame(false);
+		hamsterGame.runGame(Main::runHamster);
+		hamsterGame.stopGame();
+	}
 
-        JavaFXUI.start();
-        hamsterGame.displayInNewGameWindow();
-        hamsterGame.setInputInterface(JavaFXUI.getJavaFXInputInterface());
-    }
+	/**
+	 * Setup routine which configures the UI appropriately.
+	 * 
+	 * @param hamsterGame The hamster game to run, must not be null.
+	 */
+	private static void initUI(final HamsterGame hamsterGame) {
+		assert hamsterGame != null;
 
-    /** This operation captures the algorithm executed by the hamster.
-     * @param territory The territory of the example. It has to contain
-     *                  the default hamster. Must not be null.
-     */
-    private static void runHamster(final Territory territory) {
-        assert territory != null;
+		JavaFXUI.start();
+		hamsterGame.displayInNewGameWindow();
+		hamsterGame.setInputInterface(JavaFXUI.getJavaFXInputInterface());
+	}
 
-        final Hamster paule = territory.getDefaultHamster();
-                
-        studentTask(paule);
-        
-        
-//        paule.readNumber("15");
-//        paule.readString("Hallöchen Popöchen");
-        
-    }
-//
-//    
-//    /**
-//     * This operation configures a hamster territory via API. It is a demo
-//     * of the territory builder class.
-//     * @param hamsterGame The hamster game to run, must not be null.
-//     * @return A {@see TerritoryBuilder} object containing the information how to
-//     *         initialize this example's territory.
-//     */
-//    private static TerritoryBuilder createTerritory(final HamsterGame hamsterGame) {
-//        assert hamsterGame != null;
-//
-//        final TerritoryBuilder builder = hamsterGame.getNewTerritoryBuilder();
-//
-//        builder.initializeTerritory(EXAMPLE_TERRITORY_SIZE);
-//        builder.defaultHamsterAt(Location.from(1, 1), Direction.EAST, 0);
-//        builder.grainAt(GRAIN_LOCATION);
-//
-//        createWall(builder, Location.ORIGIN, Location.from(0, MAX_COLUMN_COUNT - 1));
-//        createWall(builder, Location.from(1, 0), Location.from(MAX_ROW_COUNT - 2, 0));
-//        createWall(builder, Location.from(1, MAX_COLUMN_COUNT - 1),
-//                        Location.from(MAX_ROW_COUNT - 2, MAX_COLUMN_COUNT - 1));
-//        createWall(builder, Location.from(MAX_ROW_COUNT - 1, 0),
-//                        Location.from(MAX_ROW_COUNT - 1, MAX_COLUMN_COUNT - 1));
-//
-//        return builder;
-//    }
-//
-//    /**
-//     * Helper operation to create a bunch of walls in a given rectangular area.
-//     * @param builder The territory builder to use to place the wall.
-//     * @param from Upper, left corner of the rectangle.
-//     * @param to Lower, right corner of the rectangle.
-//     */
-//    private static void createWall(final TerritoryBuilder builder, final Location from, final Location to) {
-//        assert builder != null;
-//        assert from != null;
-//        assert to != null;
-//        Location.getAllLocationsFromTo(from, to).forEach(location -> builder.wallAt(location));
-//    }
+	/**
+	 * This operation captures the algorithm executed by the hamster.
+	 * 
+	 * @param territory The territory of the example. It has to contain the default
+	 *                  hamster. Must not be null.
+	 */
+	private static void runHamster(final Territory territory) {
+		assert territory != null;
 
-    /**
-     * Private constructor preventing instances of this class.
-     */
-    private Main() { }
+		final Hamster paule = territory.getDefaultHamster();
+
+		studentTask(paule);
+
+
+	}
+
+	/**
+	 * Private constructor preventing instances of this class.
+	 */
+	private Main() {
+	}
 
 }
